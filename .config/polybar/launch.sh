3170b89dc7f65ff76cd22e74bc0024edb92c7448
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-06-18 00:26:08 +0300

    :up: update(polybar): Update lwqaunch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Launch bars
 polybar example &
 
-echo "Bars launched..."
+echo "Polybar Bars launched..."
