08dab31fddd63b5ee34ef6168db759046df782de
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2025-04-14 14:30:16 +0300

    :up: update(nvim): Add keymaps to move selected lines

--- stow/nvim/.config/nvim/lua/keymaps.lua
+++ stow/nvim/.config/nvim/lua/keymaps.lua
@@ -5,3 +5,10 @@ vim.keymap.set("n", "tk", ":tabnext<CR>")
 vim.keymap.set("n", "tj", ":tabprev<CR>")
 vim.keymap.set("n", "th", ":tabfirst<CR>")
 vim.keymap.set("n", "tl", ":tablast<CR>")
+
+-- Move selected lines down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- Move selected lines up
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
