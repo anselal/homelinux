6e8d2a9dbb260abc48f0d90c31c9d314c0487b90
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2025-04-22 12:28:39 +0300

    :up: update(nvim)
    * Auto-source soulistech modules on save
    * Add more vim options
    * Add newline to soulistech/init.lua

--- stow/nvim/.config/nvim/lua/soulistech/init.lua
+++ stow/nvim/.config/nvim/lua/soulistech/init.lua
@@ -1,3 +1,17 @@
 require("soulistech.options")
 require("soulistech.lazy")
 require("soulistech.keymaps")
+
+-- Auto-source soulistech modules on save
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*/lua/soulistech/*.lua",
+  callback = function(args)
+    local mod = string.match(args.file, "lua/(.*)%.lua$")
+    if mod then
+      package.loaded[mod] = nil
+      require(mod)
+      vim.notify("Reloaded: " .. mod, vim.log.levels.INFO)
+    end
+  end,
+})
+
--- stow/nvim/.config/nvim/lua/soulistech/options.lua
+++ stow/nvim/.config/nvim/lua/soulistech/options.lua
@@ -5,6 +5,10 @@ vim.g.maplocalleader = " "
 
 -- vim.opt is equal to vim.cmd("set ...")
 
+-- Enable relative line numbers
+vim.opt.nu = true
+vim.opt.rnu = true
+
 -- Set tabs to 2 spaces
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
@@ -13,3 +17,14 @@ vim.opt.expandtab = true
 -- Enable auto indenting and set it to spaces
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 2
+
+-- Enable 24-bit color
+vim.opt.termguicolors = true
+
+-- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+vim.opt.breakindent = true
+
+-- Place a column line
+vim.opt.colorcolumn = "80"
+
+
