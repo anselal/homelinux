eff881efbd37e27c51ea27dbf2f55742ca62cbb6
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2025-04-23 12:48:50 +0300

    :up: update(nvim): Update lualine
    * Integrate LazyDo with lualine
    * Update nui.nvim

--- stow/nvim/.config/nvim/lua/plugins/lualine.lua
+++ stow/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,22 @@
 return {
   "nvim-lualine/lualine.nvim",
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'tokyonight',
-      }
+  event = "VeryLazy",
+  opts = function(_, opts)
+    -- Ensure the sections structure exists
+    opts.options = opts.options or {}
+    opts.options.theme = 'tokyonight'
+
+    opts.sections = opts.sections or {}
+    opts.sections.lualine_x = opts.sections.lualine_x or {}
+
+    -- Add the lazydo component to lualine_x
+    table.insert(opts.sections.lualine_x, {
+      function()
+        return require("lazydo").get_lualine_stats()
+      end,
+      cond = function()
+        return require("lazydo")._initialized
+      end,
     })
   end,
 }
