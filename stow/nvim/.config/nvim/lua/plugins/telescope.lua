dad2da267adbbdd3a9d2f5b8173705b02bfb6a13
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2025-04-29 12:50:25 +0300

    :up: update(nvim): Update telescope and lspconfig
    * Remove tag property from telescope so that it gets all updates

--- stow/nvim/.config/nvim/lua/plugins/telescope.lua
+++ stow/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return { 
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
+  -- tag = "0.1.8",
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
@@ -8,6 +8,7 @@ return {
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    -- vim.keymap.set('n', '<leader>fgb', builtin.git_branches, { desc = 'Telescope git branches' })
   end,
 }
 
